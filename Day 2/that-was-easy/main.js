e0e66085199f39586582bb37748141c165a14e2d
Nikith Vangala <nikith.vangala@masonohioschools.com>
2017-06-28 14:28:45 +0000

    kk

--- Day 2/that-was-easy/main.js
+++ Day 2/that-was-easy/main.js
@@ -7,3 +7,12 @@ thatWasEasy.play();
 }
 
 $("#easy").on("click", sayThatWasEasy);
+
+
+$(document).keypress(delegateKeypress);
+
+function delegateKeypress(event ){
+    if (event.charCode == 48){;
+        $("#easy").trigger("click")
+    }
+}
