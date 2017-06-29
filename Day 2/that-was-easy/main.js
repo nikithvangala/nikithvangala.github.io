4510f71b8925f5bbad694966ede9df1d862c2b25
Nikith Vangala <nikith.vangala@masonohioschools.com>
2017-06-29 11:38:14 +0000

    lit

--- Day 2/that-was-easy/main.js
+++ Day 2/that-was-easy/main.js
@@ -1,18 +1,14 @@
-
-
-function sayThatWasEasy(){
-console.log("test");
-var thatWasEasy = new Audio("that_was_easy.mp3");
-thatWasEasy.play();
+function sayThatWasEasy() {
+    var thatWasEasy = new Audio("that_was_easy.mp3");
+    thatWasEasy.play();
 }
 
 $("#easy").on("click", sayThatWasEasy);
 
-
 $(document).keypress(delegateKeypress);
 
-function delegateKeypress(event ){
-    if (event.charCode == 48){;
-        $("#easy").trigger("click")
-    }
+function delegateKeypress(event) {
+  if (event.charCode == 32) {
+    $("#easy").trigger("click");
+  }
 }
