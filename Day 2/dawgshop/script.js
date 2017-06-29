d463bbf4b711552e38d8e4358519752e3d9f9be9
Nikith Vangala <nikith.vangala@masonohioschools.com>
2017-06-29 12:44:29 +0000

    dawgitup

---
+++ Day 2/dawgshop/script.js
@@ -0,0 +1,10 @@
+document.getElementById('go').onclick = function() {
+    
+    var inputText = $('#text-input').val();
+    var dawgifiedText =  inputText + ", dawg";
+    document.getElementById('output').innerHTML = dawgifiedText;
+
+    
+};
+
+
